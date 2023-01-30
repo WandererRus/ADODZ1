b8364d392be79e7a3d1a1c74b5d36e8912369c8c
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-01-30 21:25:17 +0300

    all task dz 1

--- ADODZ1/Program.cs
+++ ADODZ1/Program.cs
@@ -93,7 +93,138 @@ namespace ADODZ1
                 Console.WriteLine("Минимальная калорийность: " + (int)command.ExecuteScalar());
                 Console.WriteLine("\n-----------------------------------------------------------\n");
                 command.CommandText = "select AVG(Calories) from fandv";
-                Console.WriteLine("Средняя калорийность: " + (int)command.ExecuteScalar());                
+                Console.WriteLine("Средняя калорийность: " + (int)command.ExecuteScalar());
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+
+                command.CommandText = "select COUNT(*) from fandv WHERE Type = 'vegetables'";
+                Console.WriteLine("Количество овощей: " + (int)command.ExecuteScalar());
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+                command.CommandText = "select COUNT(*) from fandv WHERE Type = 'fruits'";
+                Console.WriteLine("Количество фруктов: " + (int)command.ExecuteScalar());
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+                command.CommandText = "select COUNT(*) from fandv WHERE Color = 'red'";
+                Console.WriteLine("Количество фруктов и овощей красного цвета: " + (int)command.ExecuteScalar());
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+
+                /*
+                 *  ?????????????????????????????????????????????????????????????????????????????????????????????????????
+                 * 
+                 * command.CommandText = "SELECT Colors.Color,COUNT(*) " +
+                    "from fandv,(select DISTINCT(Color) from fandv) AS Colors " +
+                    "WHERE  Colors.Color = fandv.Color GROUP BY Colors.Color";
+                reader = command.ExecuteReader();
+
+                DataTable dt = reader.GetSchemaTable();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    foreach (DataColumn dc in dt.Columns)
+                    {
+                        Console.WriteLine(dc.ColumnName +" " + dr[dc]);
+                    }
+                }
+                line = 0;
+                while (reader.Read())
+                {
+                    if (line == 0)
+                    {
+                        Console.WriteLine(reader.GetName(0), reader.GetName(1));
+                    }
+                    Console.WriteLine(reader.GetString(0), reader.GetInt32(1));
+                    line++;
+                }
+                reader.Close();
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+                
+                 ?????????????????????????????????????????????????????????????????????????????????????????????????????
+                 
+                 */
+
+                command.CommandText = "select DISTINCT(Color) from fandv";
+                reader = command.ExecuteReader();
+                List<string> colors = new List<string>();
+                while (reader.Read())
+                {
+                    colors.Add(reader.GetString(0));                    
+                }
+                reader.Close();
+                string result = "";
+
+                foreach (string color in colors) 
+                {
+                    command.CommandText = "select COUNT(*) from fandv WHERE Color LIKE '" + color +"'";
+                    result += color + " : " + (int)command.ExecuteScalar() + "\n";
+                }
+                Console.WriteLine(result);
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+                command.CommandText = "select Name from fandv WHERE Calories < " + Console.ReadLine();
+                reader = command.ExecuteReader();
+
+                line = 0;
+                while (reader.Read())
+                {
+                    if (line == 0)
+                    {
+                        Console.WriteLine(reader.GetName(0));
+                    }
+                    Console.WriteLine(reader.GetString(0));
+                    line++;
+                }
+                reader.Close();
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+
+                command.CommandText = "select Name from fandv WHERE Calories > " + Console.ReadLine();
+                reader = command.ExecuteReader();
+
+                line = 0;
+                while (reader.Read())
+                {
+                    if (line == 0)
+                    {
+                        Console.WriteLine(reader.GetName(0));
+                    }
+                    Console.WriteLine(reader.GetString(0));
+                    line++;
+                }
+                reader.Close();
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+
+                command.CommandText = "select Name from fandv WHERE Calories BETWEEN " + Console.ReadLine() + "AND " + Console.ReadLine();
+                reader = command.ExecuteReader();
+
+                line = 0;
+                while (reader.Read())
+                {
+                    if (line == 0)
+                    {
+                        Console.WriteLine(reader.GetName(0));
+                    }
+                    Console.WriteLine(reader.GetString(0));
+                    line++;
+                }
+                reader.Close();
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+
+                command.CommandText = "select * from fandv WHERE Color LIKE 'red' OR Color LIKE 'yellow'";
+                reader = command.ExecuteReader();
+                line = 0;
+                while (reader.Read())
+                {
+                    if (line == 0)
+                    {
+                        Console.WriteLine($"{reader.GetName(0)} | {reader.GetName(1)} " +
+                            $"| {reader.GetName(2)} | {reader.GetName(3)} " +
+                            $"| {reader.GetName(4)}");
+                    }
+                    Console.WriteLine($"{reader.GetInt32(0)} | {reader.GetString(1)} " +
+                        $"| {reader.GetString(2)} | {reader.GetString(3)} " +
+                        $"| {reader.GetInt32(4)}");
+                    line++;
+                }
+                reader.Close();
+                Console.WriteLine("\n-----------------------------------------------------------\n");
             }
             catch ( Exception ex )
             {
